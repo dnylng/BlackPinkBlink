56a26006ee0114356a41d369452041969bf376f9
dnylng <iamdannyluong@gmail.com>
2017-06-28 16:22:22 -0400

    Renamed main view controller
    I renamed the main vc and created folders to encompass the MVC model.

---
+++ MainVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  BlackPinkBlink
+//
+//  Created by Danny Luong on 6/28/17.
+//  Copyright © 2017 dnylng. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
